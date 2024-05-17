12b83a45dc2442a73d1c61934e29663a192b3b31
Gru <78027756+doctor-gru@users.noreply.github.com>
2024-05-17 03:38:00 +0000

    feat: remove morgan

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,10 @@
 import express, { Express } from "express"
 import cookieSession from "cookie-session";
-import morgan from "morgan";
 import { winstonLogger } from "../middlewares";
 
 import cookieParser from "cookie-parser";
 
-import { ENVIRONMENT, COOKIE_KEY } from "../utils/secrets";
+import { COOKIE_KEY } from "../utils/secrets";
 
 export const configureExpress = (app: Express) => {
   app.set("view engine", "ejs");
